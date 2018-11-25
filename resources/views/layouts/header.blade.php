56c75ee68ec1b037eb0f95216a1e2fb2093ef301
Talmon <talmon.mwakesi@roamtech.com>
2018-11-25 16:01:40 +0300

    Logo updated

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 
     <nav class="navbar navbar-expand-md">
     <a class="navbar-brand" href="#">
-    <img src="/img/logo.jpg" alt="" >
+    <img src="/img/ratc-logo.png" alt="" >
     </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="">
